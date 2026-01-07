969257af64021735d9b6c23f3b16b7cc99a19c11
saKana6535 <k.souki1101@gmail.com>
2026-01-07 16:24:07 +0900

    feat: AIテキスト変換のエンドポイントを追加

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,37 @@
+import "dotenv/config";
 import express from "express";
+import { GoogleGenAI } from "@google/genai";
+import cors from "cors";
 
 const app = express();
-const port = 3005;
+const port = process.env.BACKEND_PORT;
+const frontendOrigin = process.env.FRONTEND_ORIGIN;
+
+const gemini = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY || '',
+});
+
+app.use(cors({
+  origin: frontendOrigin,
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+}));
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.post("/transform", async (req, res) => {
+  const { inputText } = req.body;
+  console.log(inputText);
+  const response = await gemini.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: inputText
+  })
+  res.json({ output: response.text });
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}!!`);
